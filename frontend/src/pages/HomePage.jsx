e3fe53e607666cb692b8f433b40011ba31fc7d82
Jedd Salvador <salvadorjeddbenidick@gmail.com>
2026-09-22 10:34:35 +0800

    fixed loading UI in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 import api from "../lib/axios";
 import NotesNotFound from "../components/NotesNotFound";
+import { LoaderIcon } from "lucide-react";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -32,6 +33,14 @@ const HomePage = () => {
     fetchNotes();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10" />
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen">
       <Navbar />
@@ -39,10 +48,6 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && (
-          <div className="text-center text-primary py-10">Loading notes...</div>
-        )}
-
         {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
